7b3ec6b0483c2a877544812fb968dc12583a26f7
BaconBeast1 <mojo9027@gmail.com>
2024-09-21 15:59:06 +0800

    some tweaks n' shit

--- Assets/Scripts/QuitApplication.cs
+++ Assets/Scripts/QuitApplication.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+/*﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
 
@@ -10,4 +10,4 @@ public class QuitApplication : MonoBehaviour {
 		Application.Quit();
 	}
 
-}
+}*/
